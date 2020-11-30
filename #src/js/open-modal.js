75b47ff80824aa5eb9a63ffe8e42c4b295d4df08
Mari <33049628+Melante@users.noreply.github.com>
2020-11-30 12:56:35 +0500

    Add files via upload

--- #src/js/open-modal.js
+++ #src/js/open-modal.js
@@ -1,12 +1,26 @@
 let button = document.querySelector('.footer__button');
-let formOpen = document.querySelector('.modal-form');
+let formOpen = document.querySelector('.modal-center');
 let closeButton = formOpen.querySelector('.modal-close');
+let body = document.querySelector('body');
 
 button.addEventListener('click', function(evt){
 	evt.preventDefault();
 	formOpen.classList.add('open');
+	body.style.overflow = "hidden";
+
 });
 
 closeButton.addEventListener('click', function(){
 	formOpen.classList.remove('open');
+	body.style.overflow = "visible";
+
 });
+
+formOpen.addEventListener('click', function(){
+	formOpen.classList.remove('open');
+	body.style.overflow = "visible";
+
+});
+
+
+
